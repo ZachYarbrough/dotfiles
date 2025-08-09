edd1ce89aef754cd89de44751c6379680e64e9ef
ZachYarbrough <zachyarbro@gmail.com>
2025-08-09 14:23:42 -0500

    Updated lsp files and neovim

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -5,14 +5,6 @@ return {
       require('mason').setup({})
     end
   },
-   {
-    'williamboman/mason-lspconfig.nvim',
-    config = function()
-      require('mason-lspconfig').setup({
-        ensure_installed = { 'lua_ls', 'ts_ls', 'tailwindcss' }
-      })
-    end
-  },
   {
     'neovim/nvim-lspconfig',
     dependencies = {
@@ -22,11 +14,19 @@ return {
       local capabilities = require('blink.cmp').get_lsp_capabilities()
       local lspconfig = require('lspconfig')
       lspconfig.lua_ls.setup({ capabilities = capabilities })
-      lspconfig.ts_ls.setup({})
+      lspconfig.ts_ls.setup({ capabilities = capabilities })
 
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
       vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
     end
   },
+   {
+    'williamboman/mason-lspconfig.nvim',
+    config = function()
+      require('mason-lspconfig').setup({
+        ensure_installed = { 'lua_ls', 'ts_ls', 'tailwindcss' }
+      })
+    end
+  }
 }
 
