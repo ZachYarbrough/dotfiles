5f6123007cd4d8351ce75ab6fd473a23ac7661bf
ZachYarbrough <zachyarbro@gmail.com>
2025-08-11 10:41:15 -0500

    lsp - Removed warning from 'vim' as a global variable when writing in Lua

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,32 +1,42 @@
 return {
-  {
-    'williamboman/mason.nvim',
-    config = function()
-      require('mason').setup({})
-    end
-  },
-  {
-    'neovim/nvim-lspconfig',
-    dependencies = {
-	'saghen/blink.cmp'
+    {
+	'williamboman/mason.nvim',
+	config = function()
+	    require('mason').setup({})
+	end
     },
-    config = function()
-      local capabilities = require('blink.cmp').get_lsp_capabilities()
-      local lspconfig = require('lspconfig')
-      lspconfig.lua_ls.setup({ capabilities = capabilities })
-      lspconfig.ts_ls.setup({ capabilities = capabilities })
+    {
+	'neovim/nvim-lspconfig',
+	dependencies = {
+	    'saghen/blink.cmp'
+	},
+	config = function()
+	    local capabilities = require('blink.cmp').get_lsp_capabilities()
+	    local lspconfig = require('lspconfig')
+	    lspconfig.lua_ls.setup({ 
+		capabilities = capabilities,
+		settings = {
+		    Lua = {
+			diagnostics = {
+			    -- sets vim as a global variable for Lua
+			    globals = { "vim" },
+			},
+		    },
+		},
+	    })
+	    lspconfig.ts_ls.setup({ capabilities = capabilities })
 
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-      vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
-    end
-  },
-   {
-    'williamboman/mason-lspconfig.nvim',
-    config = function()
-      require('mason-lspconfig').setup({
-        ensure_installed = { 'lua_ls', 'ts_ls', 'tailwindcss' }
-      })
-    end
-  }
+	    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+	    vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
+	end
+    },
+    {
+	'williamboman/mason-lspconfig.nvim',
+	config = function()
+	    require('mason-lspconfig').setup({
+		ensure_installed = { 'lua_ls', 'ts_ls', 'tailwindcss' },
+	    })
+	end
+    }
 }
 
