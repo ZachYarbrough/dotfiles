c28a8dfb3466d80535c6a61192b2d96da38157cb
ZachYarbrough <zachyarbro@gmail.com>
2025-09-18 15:03:38 -0500

    Update lsp.lua to use new nvim 0.11 syntax

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -12,9 +12,14 @@ return {
 	},
 	config = function()
 	    local capabilities = require('blink.cmp').get_lsp_capabilities()
-	    local lspconfig = require('lspconfig')
-	    lspconfig.lua_ls.setup({ capabilities = capabilities })
-	    lspconfig.ts_ls.setup({ capabilities = capabilities })
+            local lspconfig = require('lspconfig')
+
+            vim.lsp.config("lua_ls", {
+		capabilities = capabilities
+	    })
+	    vim.lsp.config("ts_ls", {
+		capabilities = capabilities
+	    })
 
 	    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
 	    vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
