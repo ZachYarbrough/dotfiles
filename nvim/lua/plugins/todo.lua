2a196834b943e386b67d43972b4081bee06c7eb5
ZachYarbrough <zachyarbro@gmail.com>
2025-10-03 20:10:24 -0500

    fix(todo): updated to modern lua style and added realtive_line_numbers

--- nvim/lua/plugins/todo.lua
+++ nvim/lua/plugins/todo.lua
@@ -1,9 +1,6 @@
 return {
     "ZachYarbrough/todo-list.nvim",
-    config = function()
-        -- Optional: define key mapping here, if not already inside the plugin
-        vim.keymap.set('n', '<C-t>', function()
-            require("todo-list").show_todos()
-        end, { noremap = true, silent = true })
-    end,
+    opts = {
+	line_number_mode = 'relative'
+    }
 }
